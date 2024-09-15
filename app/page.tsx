6fed0ecf82df88f6ee440aa7cbfb05264d9e2ed3
Behsse <behsse.pro@gmail.com>
2024-09-15 22:20:26 +0200

    Full responsive nav + header + signature

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,10 @@ import Signature from "./component/Signature";
 
 export default function Home() {
 
+  useEffect(() => {
+    sessionStorage.setItem("isLoading", "false");
+  }, []);
+
   return (
     <main>
       <Header/>
